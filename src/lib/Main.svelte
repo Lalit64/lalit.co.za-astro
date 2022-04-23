519d03a8b6b02ab02a880ea48c5df18e0dfd4298
Lalit Yalamanchili <lalit.yalamanchili@gmail.com>
2022-04-23 14:47:02 +0200

    Fix contact form input outline

--- src/lib/Main.svelte
+++ src/lib/Main.svelte
@@ -1,3 +1,7 @@
+<script>
+    import {scrollto} from "svelte-scrollto";
+</script>
+
 <div class='flex flex-col items-center container py-16 justify-center '>
     <img
             src='/cdn/images/Frame.png' alt='profile'
@@ -11,7 +15,7 @@
     </p>
     <div class='flex space-x-4'><a
             class='hover:-translate-y-1 border-2 border-accent rounded py-2 active:scale-90 transition duration-300 text-sm md:text-base px-4 bg-accent text-white'
-            href='/#contact'>Contact Me</a><a
+            href='' use:scrollto={"#contact"}>Contact Me</a><a
             class='hover:-translate-y-1 border-2 border-accent rounded py-2 active:scale-90 transition duration-300 text-sm md:text-base px-4 hover:bg-accent hover:text-white'
             href='/blog'>My
         Blog</a>
